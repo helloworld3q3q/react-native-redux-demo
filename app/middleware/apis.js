b00fbebdac9a9e22096ca03f60c4af0aab01339c
supermanxiacheng <supermanxiacheng@163.com>
2017-08-30 10:42:52 +0800

    Create apis.js

---
+++ app/middleware/apis.js
@@ -0,0 +1,4 @@
+export const hotshow = 'https://api.douban.com/v2/movie/in_theaters';
+export const sonshow = 'https://api.douban.com/v2/movie/coming_soon';
+export const usshow = 'https://api.douban.com/v2/movie/us_box';
+export const nearcinemas = 'http://m.maoyan.com/cinemas.json';
